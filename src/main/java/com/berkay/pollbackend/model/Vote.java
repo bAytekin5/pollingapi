74ea0e1c0d27352b8e20fe23506eabebc62f05d3
Berkay Aytekin <berkay.0567@gmail.com>
2025-10-24 01:26:48 +0300

    feature: poll and vote model

---
+++ src/main/java/com/berkay/pollbackend/model/Vote.java
@@ -0,0 +1,50 @@
+package com.berkay.pollbackend.model;
+
+import com.berkay.pollbackend.model.audit.DateAudit;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "poll_id",
+                "user_id"
+        })
+})
+public class Vote extends DateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Choice choice;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Choice getChoice() {
+        return choice;
+    }
+
+    public void setChoice(Choice choice) {
+        this.choice = choice;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
