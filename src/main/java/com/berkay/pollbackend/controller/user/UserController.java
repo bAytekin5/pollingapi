cababd21947cdf34d7a6837f67733b863a34aa86
Berkay Aytekin <berkay.0567@gmail.com>
2025-10-27 15:14:26 +0300

    feature: user controller

--- src/main/java/com/berkay/pollbackend/controller/user/UserController.java
+++ src/main/java/com/berkay/pollbackend/controller/user/UserController.java
@@ -1,12 +1,17 @@
 package com.berkay.pollbackend.controller.user;
 
+import com.berkay.pollbackend.dto.user.UserIdentityAvailability;
+import com.berkay.pollbackend.dto.user.UserSummary;
 import com.berkay.pollbackend.repository.PollRepository;
 import com.berkay.pollbackend.repository.UserRepository;
 import com.berkay.pollbackend.repository.VoteRepository;
+import com.berkay.pollbackend.security.CurrentUser;
+import com.berkay.pollbackend.security.UserPrincipal;
 import com.berkay.pollbackend.service.PollService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -24,5 +29,24 @@ public class UserController {
         this.pollService = pollService;
     }
 
-    // TODO
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        return new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+
+    }
 }
