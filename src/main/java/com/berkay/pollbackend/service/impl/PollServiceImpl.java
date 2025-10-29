febfe1f06392c5e1c931bbb58ccb679fd0bb19b6
Berkay Aytekin <berkay.0567@gmail.com>
2025-10-29 03:15:05 +0300

    feat: poll service the structure created

--- src/main/java/com/berkay/pollbackend/service/impl/PollServiceImpl.java
+++ src/main/java/com/berkay/pollbackend/service/impl/PollServiceImpl.java
@@ -4,18 +4,69 @@ import com.berkay.pollbackend.dto.paged.PagedResponse;
 import com.berkay.pollbackend.dto.poll.PollRequest;
 import com.berkay.pollbackend.dto.poll.PollResponse;
 import com.berkay.pollbackend.dto.vote.VoteRequest;
+import com.berkay.pollbackend.exception.ResourceNotFoundException;
 import com.berkay.pollbackend.model.Poll;
+import com.berkay.pollbackend.model.User;
+import com.berkay.pollbackend.repository.PollRepository;
+import com.berkay.pollbackend.repository.UserRepository;
+import com.berkay.pollbackend.repository.VoteRepository;
 import com.berkay.pollbackend.security.UserPrincipal;
 import com.berkay.pollbackend.service.PollService;
+import com.berkay.pollbackend.util.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-// TODO
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 
 @Service
 public class PollServiceImpl implements PollService {
+
+    private final PollRepository pollRepository;
+    private final VoteRepository voteRepository;
+    private final UserRepository userRepository;
+
+    public PollServiceImpl(PollRepository pollRepository, VoteRepository voteRepository, UserRepository userRepository) {
+        this.pollRepository = pollRepository;
+        this.voteRepository = voteRepository;
+        this.userRepository = userRepository;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(PollServiceImpl.class);
+
     @Override
     public PagedResponse<PollResponse> getAllPolls(UserPrincipal currentUser, int page, int size) {
-        return null;
+        validatePageNumberAndSize(page, size);
+
+        // retrieve
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC, "createdAt");
+        Page<Poll> polls = pollRepository.findAll(pageable);
+
+        if (polls.getNumberOfElements() == 0) {
+            return new PagedResponse<>(Collections.emptyList(), polls.getNumber(),
+                    polls.getSize(), polls.getTotalElements(), polls.getTotalPages(), polls.isLast());
+        }
+
+        List<Long> pollIds = polls.map(Poll::getId).getContent();
+        Map<Long, Long> chociceVoteCountMap = getChoiceVoteCountMap(pollIds);
+        Map<Long, Long> pollUserVoteMap = getPollUserVoteMap(currentUser, pollIds);
+        Map<Long, User> creatorMap = getPollCreatorMap(polls.getContent());
+
+        List<PollResponse> pollResponses = polls.map(poll -> {
+            return ModelMapper.mapPollToPollResponse(poll,
+                    chociceVoteCountMap,
+                    creatorMap.get(poll.getCreatedAt()),
+                    pollUserVoteMap == null ? null : pollUserVoteMap.getOrDefault(poll.getId(), null));
+        }).getContent();
+        return new PagedResponse<>(pollResponses, polls.getNumber(),
+                polls.getSize(), polls.getTotalElements(), polls.getTotalPages(), polls.isLast());
     }
 
     @Override
@@ -32,4 +83,52 @@ public class PollServiceImpl implements PollService {
     public PollResponse castVoteAndGetUpdatedPoll(Long pollId, VoteRequest voteRequest, UserPrincipal currentUser) {
         return null;
     }
+
+    @Override
+    public PagedResponse<PollResponse> getPollsCreatedBy(String username, UserPrincipal currentUser, int page, int size) {
+        validatePageNumberAndSize(page, size);
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC, "createdAt");
+        Page<Poll> polls = pollRepository.findByCreatedBy(user.getId(), pageable);
+
+        // todo duplicate
+        if (polls.getNumberOfElements() == 0) {
+            return new PagedResponse<>(Collections.emptyList(), polls.getNumber(),
+                    polls.getSize(), polls.getTotalElements(), polls.getTotalPages(), polls.isLast());
+        }
+
+        List<Long> pollIds = polls.map(Poll::getId).getContent();
+        Map<Long, Long> choiceVoteCountMap = getChoiceVoteCountMap(pollIds);
+        Map<Long, Long> pollUserVoteMap = getPollUserVoteMap(currentUser, pollIds);
+
+        List<PollResponse> pollResponses = polls.map(poll -> {
+            return ModelMapper.mapPollToPollResponse(poll,
+                    choiceVoteCountMap,
+                    user,
+                    pollUserVoteMap == null ? null : pollUserVoteMap.getOrDefault(poll.getId(), null));
+        }).getContent();
+        return new PagedResponse<>(pollResponses, polls.getNumber(),
+                polls.getSize(), polls.getTotalElements(), polls.getTotalPages(), polls.isLast());
+    }
+
+
+    // todo map package taşı
+    private Map<Long, Long> getChoiceVoteCountMap(List<Long> pollIds) {
+        return null;
+    }
+
+    private void validatePageNumberAndSize(int page, int size) {
+    }
+
+    private Map<Long, Long> getPollUserVoteMap(UserPrincipal currentUser, List<Long> pollIds) {
+        return null;
+    }
+
+    private Map<Long, User> getPollCreatorMap(List<Poll> content) {
+
+        return null;
+    }
+
 }
